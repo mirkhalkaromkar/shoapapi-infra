75674ecec2278c3ed95e3ea3984dcada803b5047
mirkhalkaromkar <mirkhalkaromkar@gmail.com>
2026-05-24 10:31:57 +0530

    Update variables.tf

--- shopapi-infra/phase-1/modules/ec2/variables.tf
+++ shopapi-infra/phase-1/modules/ec2/variables.tf
@@ -8,4 +8,8 @@ variable "iam_instance_profile" { type = string }
 variable "db_host"              { type = string }
 variable "db_name"              { type = string }
 variable "db_username"          { type = string }
-variable "db_password"          { type = string; sensitive = true }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
