e025e90fa6f3eb7008eac18dc8906ec4815ea87b
mirkhalkaromkar <mirkhalkaromkar@gmail.com>
2026-05-24 10:33:16 +0530

    Update variables.tf

--- shopapi-infra/phase-1/modules/rds/variables.tf
+++ shopapi-infra/phase-1/modules/rds/variables.tf
@@ -2,6 +2,10 @@ variable "project"            { type = string }
 variable "env"                { type = string }
 variable "db_name"            { type = string }
 variable "db_username"        { type = string }
-variable "db_password"        { type = string; sensitive = true }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
 variable "private_subnet_ids" { type = list(string) }
 variable "db_sg_id"           { type = string }
+
