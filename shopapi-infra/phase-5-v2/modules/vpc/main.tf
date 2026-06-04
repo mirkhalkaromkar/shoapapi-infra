d6250e23ea965a538a377789ea72044b5a3e00c6
mirkhalkaromkar <mirkhalkaromkar@gmail.com>
2026-06-04 09:55:54 +0530

    Update main.tf

--- shopapi-infra/phase-5-v2/modules/vpc/main.tf
+++ shopapi-infra/phase-5-v2/modules/vpc/main.tf
@@ -29,7 +29,10 @@ resource "aws_subnet" "private" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.igw.id }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = { Name = "${var.project}-${var.env}-public-rt", Project = var.project }
 }
 
